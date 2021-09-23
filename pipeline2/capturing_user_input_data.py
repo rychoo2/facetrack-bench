042b7502e883afe9c8c67e90b43af4fc5d9391de
Antoni Wozniak <wozniak.antoni@gmail.com>
2021-09-23 15:04:12 +0100

    added option to quit on ESC

--- pipeline2/capturing_user_input_data.py
+++ pipeline2/capturing_user_input_data.py
@@ -8,8 +8,8 @@ from libs.utils import get_timestamp
 n = 5
 radius = 2.5
 
-# A Halton sequence is a sequence of points within a unit dim-cube which 
-# have low discrepancy (that is, they appear to be randomish but cover 
+# A Halton sequence is a sequence of points within a unit dim-cube which
+# have low discrepancy (that is, they appear to be randomish but cover
 # the domain uniformly)
 def halton(dim: int, nbpts: int):
     h = numpy.full(nbpts * dim, numpy.nan)
@@ -33,30 +33,36 @@ def halton(dim: int, nbpts: int):
     return h.reshape(nbpts, dim)
 
 # Generate list of n points xy coordinates for given screen dimensions
-def points(n,screen_w,screen_h):
+def points(n, screen_w, screen_h):
     hlt = halton(2, n)
-    points = (hlt*(screen_w,screen_h)).astype(int)
+    points = (hlt*(screen_w, screen_h)).astype(int)
     return points.tolist()
 
 root = tk.Tk()
-root.frame=0
-screen_width =root.winfo_screenwidth()
-screen_height =root.winfo_screenheight()
+root.frame = 0
+screen_width = root.winfo_screenwidth()
+screen_height = root.winfo_screenheight()
 # Create full screen canvas
 root.state('zoomed')
 root.attributes('-fullscreen', True)
 canvas = tk.Canvas(root, bg='black', highlightthickness=0)
 canvas.pack(fill=tk.BOTH, expand=True)
 
-pts = points(n,screen_width,screen_height)
+pts = points(n, screen_width, screen_height)
 points_iterator = iter(pts)
 
-cam=cv2.VideoCapture(0)
-now=get_timestamp()
-output ='./data/raw/{}'.format(now)
+cam = cv2.VideoCapture(0)
+now = get_timestamp()
+output = './data/raw/{}'.format(now)
 os.makedirs(output+'/images')
-fw=open('{}/positions.csv'.format(output),'w')
+fw = open('{}/positions.csv'.format(output), 'w')
 fw.write("frame,timestamp,x,y,screen_width,screen_height,image_path,image_width,image_height\n")
+
+def close(e):
+    cam.release()
+    root.destroy()
+
+root.bind('<Escape>', close)
 
 def circle(x, y, radius, canvasName, color="yellow"):
     x0 = x-radius
@@ -65,27 +71,32 @@ def circle(x, y, radius, canvasName, color="yellow"):
     y1 = y+radius
     return canvasName.create_oval(x0, y0, x1, y1, fill=color, outline=color, tags="circle")
 
+
 def draw(canvasName):
     try:
         p = next(points_iterator)
         circle(p[0], p[1], radius, canvasName)
         canvas.tag_bind("circle", "<1>", on_click)
     except StopIteration:
-        cam.release()
-        root.destroy()
-        
+        close(StopIteration)
+
+
 def on_click(event):
-    root.frame+=1
-    timestamp=get_timestamp()
-    x,y=event.x,event.y
-    ret_val,img=cam.read()
-    img_height,img_width=img.shape[0:2]
-    image_path="images/{}_{:03d}.jpg".format(timestamp,root.frame)
-    fw.write("{},{},{},{},{},{},{},{},{}\n".format(root.frame,timestamp,x,y,screen_width,screen_height,image_path,img_width,img_height))
+    root.frame += 1
+    timestamp = get_timestamp()
+    x, y = event.x, event.y
+    ret_val, img = cam.read()
+    img_height, img_width = img.shape[0:2]
+    image_path = "images/{}_{:03d}.jpg".format(timestamp, root.frame)
+    fw.write("{},{},{},{},{},{},{},{},{}\n".format(root.frame, timestamp,
+             x, y, screen_width, screen_height, image_path, img_width, img_height))
     fw.flush()
-    cv2.imwrite("{}/{}".format(output,image_path),img)
+    cv2.imwrite("{}/{}".format(output, image_path), img)
     canvas.delete("circle")
     draw(canvas)
+
+
+
 
 if __name__ == "__main__":
     draw(canvas)
