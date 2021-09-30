0214eadac466300295719c34e71d99d749eb7aa6
Juliusz Skowron <juliusz.skowron@kuehne-nagel.com>
2021-09-30 22:27:18 +0200

    adding test to set up expectations for drawing prediction markers function

--- test/test_pipeline2_machine_learning.py
+++ test/test_pipeline2_machine_learning.py
@@ -1,9 +1,11 @@
 import shutil
 from unittest import TestCase
 from pipeline2.step3_machine_learning import generate_predictions_for_datasets
+
 import time
 import pandas as pd
 from pandas.testing import assert_frame_equal
+import os
 
 class TestPrediction_models(TestCase):
     test_output_path = "tmp/pipeline2/machine_learning/1"
@@ -36,7 +38,33 @@ class TestPrediction_models(TestCase):
         self.assertLess(duration, 150)
 
 
+    def test_drawing_predictions(self):
+        generate_predictions_for_datasets("data/pipeline2", self.test_output_path)
+        predictions_csv = "{}/models_predictions.csv".format(self.test_output_path)
+        from libs.prediction_markers import draw_prediction_markers
+        draw_prediction_markers(predictions_csv)
+
+        self.assertEqual(set(os.listdir("{}/capture0/images".format(self.test_output_path))),
+                         {'frame_6.jpg',
+                          'frame_12.jpg',
+                          'frame_13.jpg',
+                          'frame_7.jpg',
+                          'frame_5.jpg',
+                          'frame_11.jpg',
+                          'frame_10.jpg',
+                          'frame_4.jpg',
+                          'frame_14.jpg',
+                          'frame_15.jpg',
+                          'frame_1.jpg',
+                          'frame_3.jpg',
+                          'frame_17.jpg',
+                          'frame_16.jpg',
+                          'frame_2.jpg',
+                          'frame_9.jpg',
+                          'frame_8.jpg'})
+
+
+
     def readfile(self, path):
         with open(path) as f:
             return f.readlines()
-
