d51bab72926fe525c86eff47afb140e512182cf5
Antoni Wozniak <wozniak.antoni@gmail.com>
2021-10-04 21:37:19 +0100

    refactor drawing markers

--- libs/draw_utils.py
+++ libs/draw_utils.py
@@ -1,53 +1,49 @@
 import cv2
 import os
 
-colors = {"orange": (10, 121, 255), "green": (0, 252, 124), "red": (0, 0, 255)}
-
 
 def calculate_screen_position(x, y, img_width, img_height):
     return int((1 - x) * img_width), int(y * img_height)
 
 
-def draw_marker(img, x, y, model_id, type):
-    r1 = 2
-    r2 = 15
-    if type == "train":
-        color = colors["orange"]
-    elif type == "test":
-        color = colors["red"]
-    elif type == 'target':
+def draw_marker(img, x, y, number, color):
+    colors = {"orange": (10, 121, 255), "green": (0, 252, 124), "red": (0, 0, 255)}
+    c = colors[color]
+
+    if number is None:
         r1 = 3
         r2 = 18
-        color = colors["green"]
-    cv2.circle(img, (x, y), r1, color, -1, cv2.LINE_AA)
-    cv2.circle(img, (x, y), r2, color, 2, cv2.LINE_AA)
-    cv2.putText(img, model_id, (x + 3, y + 4), cv2.FONT_HERSHEY_SIMPLEX, .4,
-                color, 1, cv2.LINE_AA)
+        number = ''
+    else:
+        r1 = 2
+        r2 = 15
+        number = str(number)
+    cv2.circle(img, (x, y), r1, c, -1, cv2.LINE_AA)
+    cv2.circle(img, (x, y), r2, c, 2, cv2.LINE_AA)
+    cv2.putText(img, number, (x + 3, y + 4), cv2.FONT_HERSHEY_SIMPLEX, .4,
+                c, 1, cv2.LINE_AA)
 
 
-def draw_markers_on_img(pp):
-    img = cv2.imread(pp["file_path"])
+def draw_markers_on_img(marker):
+    img = cv2.imread(marker["img_path"])
     img_height, img_width = img.shape[:2]
-    target_x, target_y = pp["target_x"], pp["target_y"]
-    draw_marker(img, *calculate_screen_position(target_x, target_y, img_width, img_height), "", "target")
-    for m in pp["models"]:
+    for m in marker["markers"]:
         x, y = calculate_screen_position(
-            m["prediction_x"], m["prediction_y"], img_width, img_height)
-        model_id = str(m["model_id"])
-        pr_type = m["type"]
-        draw_marker(img, x, y, model_id, pr_type)
+            m["x"], m["y"], img_width, img_height)
+        number = m["number"]
+        color = m["color"]
+        draw_marker(img, x, y, number, color)
     return img
 
 
-def place_markers_on_images(data, path):
-    output_path = "{}/images".format(path)
+def place_markers_on_images(data, output_path):
     if not os.path.exists(output_path):
         os.makedirs(output_path)
     for entry in data:
-        img_dir = "{}/{}".format(output_path,entry["file_path"].split("/")[-3])
+        img_dir = "{}/{}/{}".format(output_path, entry["img_path"].split("/")[-3], "images")
         if not os.path.exists(img_dir):
             os.makedirs(img_dir)
-        img_file = entry["file_path"].split('/')[-1]
+        img_file = entry["img_path"].split('/')[-1]
         img = draw_markers_on_img(entry)
         cv2.imwrite(f"{img_dir}/{img_file}", img)
         cv2.destroyAllWindows()
---
+++ libs/prediction_markers.py
@@ -0,0 +1,45 @@
+import os
+import pandas as pd
+from .draw_utils import place_markers_on_images
+
+input_path = "../test/tmp/pipeline2/machine_learning/1/models_predictions.csv"
+
+
+def prepare_data_from_predictions_csv(csv_path):
+    df = pd.read_csv(csv_path)
+    df = df[~df['dataset'].str.startswith('overall')]  # drop rows which starts with overall...
+    unique_files = df['landmark_image'].unique()  # find unique image filenames
+    data = []
+    for u_file in unique_files:  # prepare data for drawing function
+        u_file_df = df.loc[df["landmark_image"] == u_file]
+        # dataset = u_file_df.iloc[0, 0]
+        target_x = u_file_df.iloc[0, 9]
+        target_y = u_file_df.iloc[0, 10]
+        markers = [{"x": target_x, "y": target_y, "number": None, "color": "green"}]
+        for row in u_file_df.itertuples():
+            if row.type == "train":
+                color = "orange"
+            elif row.type == "test":
+                color = "red"
+            markers.append({
+                "number": row.model_id,
+                "color": color,
+                "x": row.prediction_x,
+                "y": row.prediction_y
+            })
+        img_predictions_data = {
+            "img_path": f"../test/{u_file}",
+            "markers": markers
+        }
+        data.append(img_predictions_data)
+    return data
+
+
+def draw_prediction_markers(csv_path):
+    output_path = os.path.dirname(os.path.relpath(csv_path))
+    data = prepare_data_from_predictions_csv(csv_path)
+    place_markers_on_images(data, output_path)
+
+
+if __name__ == '__main__':
+    draw_prediction_markers(input_path)
